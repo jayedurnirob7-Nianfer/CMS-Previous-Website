ba1df0a7ded65987d54f549c5049c66384b6d38f
jayedurnirob7-Nianfer <jayedurnirob7@gmail.com>
2026-08-04 04:20:19 +0600

    feat: add website favicon and icon metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,11 @@ import './globals.css'
 export const metadata = {
   title: 'CMS Previous Website',
   description: 'Manage your website entries easily',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/icon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
